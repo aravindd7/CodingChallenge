35254d4037488acae1b97f7381087579d2001e78
hosead6168 <dominique11h@yahoo.com>
2021-08-05 05:58:48 -0500

    Added binary search

--- searching/binarysearch/binarysearchFirstOccurence.js
+++ searching/binarysearch/binarysearchFirstOccurence.js
@@ -1,17 +1,19 @@
 function binarySearch(array, target) {
     let lo = 0;
     let high = array.length - 1;
-    while (lo <= high) {
+    while (lo < high) {
         let mid = (lo + high) / 2;
         if (target > array[mid]) {
-            lo = mid + 1;
-        } else if (target < array[mid]) {
-            high = mid - 1;
+            lo = mid + 1; // search the right half [mid + 1, high]
         } else {
-            return mid;
-        }
+            high = mid; // search the left half [lo, mid]
+        } 
     }
-    return -1;
+        if (array[lo] === target) {
+            return lo;
+        } else {
+        return -1;
+    }
 }
 
 let array = [1,3,4,6,7,8,11,15,22,25,33]
